c76427daedc9a2f02792540762ec5277ec106fd3
Quentin Maia <q.maia@laposte.net>
2025-09-22 06:24:05 +0200

    Update exo4.php

--- Quentin_MAIA/AlgoPHP/Partie 1/exo4.php
+++ Quentin_MAIA/AlgoPHP/Partie 1/exo4.php
@@ -3,11 +3,14 @@
 <p>Ecrire un algorithme permettant de savoir si une phrase est un palindrome.</p>
 <h2>Résultat</h2>
 <?php
-$phrase = "engage le jeu que je le gagne";
-$phraseNoSpace= str_replace(" ","",$phrase);
-$revPhrase= strrev($phraseNoSpace);
-if ($phraseNoSpace == $revPhrase)
-    
-    echo " La phrase $phrase est un palindrome.";
-else echo "La phrase $phrase n'est pas un palindrome.";
+$phrase = "Engage le jeu que je le gagne";
+$phraseMin = strtolower($phrase); // Tout en minuscule
+$phraseNoSpace = str_replace(" ", "", $phraseMin); // Sans espace
+$revPhrase = strrev($phraseNoSpace); // Phrase inversée
+
+if ($phraseNoSpace == $revPhrase) {
+    echo "La phrase '$phrase' est un palindrome.";
+} else {
+    echo "La phrase '$phrase' n'est pas un palindrome.";
+}
  ?>
