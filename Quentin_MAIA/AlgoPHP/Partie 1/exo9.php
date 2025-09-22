42073d4f7b75243b3804ac3b76ad9980b8e51365
Quentin Maia <q.maia@laposte.net>
2025-09-22 05:07:02 +0200

    Add files via upload

---
+++ Quentin_MAIA/AlgoPHP/Partie 1/exo9.php
@@ -0,0 +1,25 @@
+<h1>Exercice 9</h1>
+
+<p>Nous souhaitons savoir si une personne est imposable en fonction de son age et de son sexe. Si la personne est une femme dont l'age est compris entre 18 et 35 ansou si c'est un homme de plus de 20ans,
+     alors celle-ci est imposable (sinon ce n'est pas le cas, " non imposable ").</p>
+
+<h2>Résultat</h2>
+<?php
+
+function imposable($age, $sexe){
+    if ($sexe == "f" && $age >= 18 && $age <= 35) {
+        echo "La personne est imposable";
+    } elseif ($sexe == "h" && $age >= 20) {
+        echo "La personne est imposable";
+    } else {
+        echo "La personne n'est pas imposable";
+    }
+}
+
+$age = 32;
+$sexe = "f";
+echo "Age : $age";
+echo "<br>";
+echo "Sexe : $sexe";
+echo "<br>";
+imposable($age, $sexe);
