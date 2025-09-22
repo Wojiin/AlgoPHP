aa625c825b83d487028985218c4be5c1838ea0c6
Quentin Maia <q.maia@laposte.net>
2025-09-22 05:06:05 +0200

    Add files via upload

---
+++ Quentin_MAIA/AlgoPHP/Partie 1/exo5.php
@@ -0,0 +1,11 @@
+<h1>Exercice 5</h1>
+
+<p>Ecrire un algorithme qui déclare une valeur en francs et qui la convertit en euros.</p>
+<h2>Résultat</h2>
+
+<?php
+$montant = 100;
+$conversion_euro = ($montant/6.55957);
+$montant_euro = round($conversion_euro,2);
+echo "Montant en francs : $montant<br>";
+echo "$montant francs = $montant_euro €";
