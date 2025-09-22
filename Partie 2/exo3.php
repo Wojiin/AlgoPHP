79bd622de222fc95264d895290c508cb833bc9aa
Quentin Maia <q.maia@laposte.net>
2025-09-22 16:56:43 +0200

    Delete Partie 2 directory

--- Partie 2/exo3.php
+++
@@ -1,8 +0,0 @@
-<h1>Exercice 3</h1>
-
-<p>Afficher un lien hypertexte permettant d'accéder au site d'Elan Formation. Le lien devra s'ouvrir dans un nouvel (onglet_blank).</p>
-
-<h2>Résultat</h2>
-<?php
-echo "<a href='https://www.elan-formation.fr'target=blank>Elan-Formation</url>";
-
